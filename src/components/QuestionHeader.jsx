97a85e746103f10c29b83e2c53e6d150fe3ac3bb
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-05 20:05:23 +0300

    add: question header to display question

---
+++ src/components/QuestionHeader.jsx
@@ -0,0 +1,11 @@
+const QuestionHeader=({children,question})=>{
+    return(
+        <div className="block bg-green-600 p-4 m-4  rounded-md">
+            <div>
+                {question} 
+            </div>
+
+        </div>
+    )
+}
+export default QuestionHeader;
