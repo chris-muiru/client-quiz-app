3ae426fe5c37fdc7ff955374e7a7bbdf1078e52e
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-05 13:22:35 +0300

    refactor: replace for loop with map function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Main from "./components/Main";
 import "./assets/output.css";
 import Footer from "./components/Footer";
+import Question from "./components/Question";
+import { useParams } from "react-router-dom";
 function App() {
 	return (
 		<div className="w-full bg-slate-700 block fixed h-full">
 			<Header />
 			<NavBar />
-			<Main />
-			<Footer/>
+			<Router>
+				<Routes>
+					<Route path="" element={<Main />} />
+					<Route
+						path="/quiz/:id"
+						element={
+							<>
+								<Question id=""/>
+							</>
+						}
+					/>
+				</Routes>
+			</Router>
+			<Footer />
 		</div>
 	);
 }
