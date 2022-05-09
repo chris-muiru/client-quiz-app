a6f8b313b4edd795a46b44c3b1f5335a9c05dcc4
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-10 01:21:22 +0300

    add: add routes for navigation and authentication

---
+++ src/Routes/AuthRoutes.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const AuthRoutes = () => {
+	return <Routes></Routes>;
+};
+
+export default AuthRoutes;
