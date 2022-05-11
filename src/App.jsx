a58436ff8b387ea542c79a162644ef4e75636006
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-11 14:24:52 +0300

    add: add router that checks authentication and logs user if authenticated else redirected to login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from "react-router-dom";
 import "./assets/output.css";
 import Main from "./Main";
 import Login from "./components/authentication/LogIn";
 import Header from "./components/Header";
-
+// import PrivateRoute from "./utils/PrivateRoute";
+import { useAuthContext } from "./AuthProvider";
 const App = () => {
+	let { user } = useAuthContext();
 	return (
 		<div className="w-full bg-slate-700 block fixed h-full">
 			<Header />
 			<Router>
 				<Routes>
-					<Route path="dash/" element={<Main />} />
-					<Route path="auth/" element={<Login />} />
+					<Route
+						path="/dash/*"
+						element={!user ? <Navigate to="/auth" /> : <Main />}
+					></Route>
+					<Route path="/auth" element={<Login />} />
 				</Routes>
 			</Router>
 		</div>
