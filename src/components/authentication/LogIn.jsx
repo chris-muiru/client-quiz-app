c11b2416a1e85c080f2fdda561a9648f68fc056a
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-11 14:53:14 +0300

    add: login context to form onSubmit event

--- src/components/authentication/LogIn.jsx
+++ src/components/authentication/LogIn.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-
+import { useAuthContext } from "./../../AuthProvider";
 const LogIn = () => {
+	let { loginUser } = useAuthContext();
 	return (
-		<div className="bg-slate-600  sm:w-1/2 h-1/2 m-auto mt-20 box-border text-center">
+		<div className="bg-slate-600  sm:w-1/2 h-1/2 m-auto mt-20 box-border text-center sm:rounded-md">
 			<h1 className="font-semibold text-white text-lg pt-6 ">
 				Sign in to your account
 			</h1>
 			<div className="text-center m-10 p-5 box-border">
-				<form method="post">
+				<form method="post" onSubmit={loginUser}>
 					<input
 						type="text"
 						name="username"
@@ -23,8 +24,8 @@ const LogIn = () => {
 					/>
 					<br />
 					<button
-						className="bg-green-500 border-none rounded-md p-3 w-44"
 						type="submit"
+						className="bg-green-500 border-none rounded-md p-3 w-44"
 					>
 						Submit
 					</button>
