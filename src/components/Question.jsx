8b8cfee1da78b111d1c30ab449e5311a687c41ce
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-11 15:00:33 +0300

    add: add gettoken context to bearer header in fetch method

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,8 +1,11 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useAuthContext } from "../AuthProvider";
 const Question = () => {
+	let [questionDetails, setQuestionDetails] = useState({});
 	let { id } = useParams();
-	let [questionDetails, setQuestionDetails] = useState("");
+	let { getToken } = useAuthContext();
+
 	let fetchQuestionSelected = async () => {
 		let URL = `http://localhost:8000/quiz/${id}`;
 		let data = await fetch(URL, {
@@ -10,20 +13,22 @@ const Question = () => {
 			mode: "cors",
 			headers: {
 				"Content-Type": "application/json",
+				Authorization: `Bearer ${getToken()}`,
 			},
 		});
 		data = await data.json();
-		setQuestionDetails(data[0]);
-	};
-	let { question, answer, discussion } = questionDetails;
 
+		setQuestionDetails(data);
+	};
 	useEffect(() => {
 		fetchQuestionSelected();
 	}, []);
 	console.log(questionDetails);
+	let { answer } = questionDetails;
+	console.log(answer);
 	return (
 		<div className="mt-20 text-white text-center w-3/4 m-auto rounded-sm">
-			<div>{question}</div>
+			{questionDetails.question}
 		</div>
 	);
 };
