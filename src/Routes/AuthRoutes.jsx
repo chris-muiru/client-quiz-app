59fd0c314f1f5fdd664b8d715ee32621072c81e5
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-11 15:03:03 +0300

    delete file

--- src/Routes/AuthRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-const AuthRoutes = () => {
-	return <Routes></Routes>;
-};
-
-export default AuthRoutes;
