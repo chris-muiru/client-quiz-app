9fec6f697b368c0f58276cd93326ca51cc697a33
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-17 13:25:12 +0300

    add: update file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
 const Footer = () => {
-    /**
-     * a footer 
-     */
+	/**
+	 * a footer
+	 */
 	return (
 		<div className="bg-black absolute bottom-0 w-full">
-			<div className="">
-			</div>
+			<div className=""></div>
 			<div class="bg-black text-white text-center">
 				<span>2021 Copyright: kris muiru</span>
 			</div>
