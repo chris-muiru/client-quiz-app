1e114cf2b0d3809f94547b3dbf36abc55e18c1a9
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-30 03:12:17 +0300

    add: fetch score data and siplay it on Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,36 @@
 import { useAuthContext } from "../AuthProvider";
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState, useEffect, useRef } from "react";
 const NavBar = () => {
-	let { logoutUser, user } = useAuthContext();
-	console.log(user);
+	let { logoutUser, getToken } = useAuthContext();
+	let [score, setScore] = useState(0);
+	const fetchScore = async () => {
+		let URL = "http://localhost:8000/quiz/score/";
+		let response = await fetch(URL, {
+			method: "GET",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${getToken()}`,
+			},
+		});
+		let { total } = await response.json();
+		setScore(total);
+	};
+	useEffect(() => {
+		fetchScore();
+	}, []);
 	return (
 		<div className="">
 			<nav className="p-4 rounded-md">
-				<div className="absolute right-5 px-10">
+				<div className="px-10 m-auto flex justify-between w-3/4">
+					<div className="relative right-10 pt-2 text-green-600 font-bold">
+						Score: <span className="text-white">{score}</span>
+					</div>
 					<div
-						className="rounded-2xl text-white border-green-700 hover:border-2 box-border"
+						align="center"
+						className="bg-transparent rounded-3xl absolute  right-1/4 hover:bg-green-700  text-white border-white hover:border-2 box-border"
 						onClick={logoutUser}
 					>
 						<button className="m-3">
