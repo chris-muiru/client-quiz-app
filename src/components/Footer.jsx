d108153fed11454e58e83826c72903dfb741f3cd
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-06-02 22:45:59 +0300

    add: change name of author

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 		<div className="bg-black absolute bottom-0 w-full">
 			<div className=""></div>
 			<div class="bg-black text-white text-center">
-				<span>2021 Copyright: kris muiru</span>
+				<span>2021 Copyright: Chris Muiru</span>
 			</div>
 		</div>
 	);
